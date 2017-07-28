eb7a30091bef120d62588e018341bd6ad36ae271
Adam Heald <adamheald13@gmail.com>
2017-07-28 01:15:11 -0500

    startup time + vscode launch config

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.time('Startup time');
+
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
@@ -53,3 +55,4 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+console.timeEnd('Startup time');
